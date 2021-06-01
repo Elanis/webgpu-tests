6db2532e539e743db2ebf9ae3d683a3f9d175935
Elanis <elanis@hotmail.com>
2021-06-01 21:17:06 +0200

    Clean

--- js/index.js
+++ js/index.js
@@ -1,39 +1,39 @@
 const SWAP_CHAIN_FORMAT = 'bgra8unorm';
 
 const FRAGMENT_SHADER = `
-    [[stage(fragment)]]
-    fn main([[location(0)]] inColor : vec3<f32>) -> [[location(0)]] vec4<f32> {
-        return vec4<f32>(inColor, 1.0);
-    }
+	[[stage(fragment)]]
+	fn main([[location(0)]] inColor : vec3<f32>) -> [[location(0)]] vec4<f32> {
+		return vec4<f32>(inColor, 1.0);
+	}
 `;
 
 const VERTEX_SHADER = `
-    let positions : array<vec2<f32>, 3> = array<vec2<f32>, 3>(
-	    vec2<f32>(0.0, 0.5),
-	    vec2<f32>(-0.5, -0.5),
-	    vec2<f32>(0.5, -0.5)
-    );
+	let positions : array<vec2<f32>, 3> = array<vec2<f32>, 3>(
+		vec2<f32>(0.0, 0.5),
+		vec2<f32>(-0.5, -0.5),
+		vec2<f32>(0.5, -0.5)
+	);
 
-    let colors : array<vec3<f32>, 3> = array<vec3<f32>, 3>(
-	    vec3<f32>(1.0, 0.0, 0.0),
-	    vec3<f32>(0.0, 1.0, 0.0),
-	    vec3<f32>(0.0, 0.0, 1.0)
-    );
+	let colors : array<vec3<f32>, 3> = array<vec3<f32>, 3>(
+		vec3<f32>(1.0, 0.0, 0.0),
+		vec3<f32>(0.0, 1.0, 0.0),
+		vec3<f32>(0.0, 0.0, 1.0)
+	);
 
 	struct VertexOutput {
-	    [[builtin(position)]] Position : vec4<f32>;
-	    [[location(0)]] fragColor : vec3<f32>;
+		[[builtin(position)]] Position : vec4<f32>;
+		[[location(0)]] fragColor : vec3<f32>;
 	};
   
-    [[stage(vertex)]]
-    fn main([[builtin(vertex_idx)]] VertexIndex : i32) -> VertexOutput {
-    	var vertexOutput : VertexOutput;
+	[[stage(vertex)]]
+	fn main([[builtin(vertex_idx)]] VertexIndex : i32) -> VertexOutput {
+		var vertexOutput : VertexOutput;
 
-        vertexOutput.Position = vec4<f32>(positions[VertexIndex], 0.0, 1.0);
-        vertexOutput.fragColor = colors[VertexIndex];
+		vertexOutput.Position = vec4<f32>(positions[VertexIndex], 0.0, 1.0);
+		vertexOutput.fragColor = colors[VertexIndex];
 
-        return vertexOutput;
-    }
+		return vertexOutput;
+	}
 `;
 
 
