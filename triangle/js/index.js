54c5eb5f9f2969e6fcf07f34acf2ecf1694d1957
Elanis <elanis@hotmail.com>
2022-11-18 17:54:15 +0100

    Update with latest WebGPU spec

--- triangle/js/index.js
+++ triangle/js/index.js
@@ -1,32 +1,32 @@
-const SWAP_CHAIN_FORMAT = 'bgra8unorm';
+const PRESENTATION_FORMAT = navigator.gpu.getPreferredCanvasFormat();
 
 const FRAGMENT_SHADER = `
-	[[stage(fragment)]]
-	fn main([[location(0)]] inColor : vec3<f32>) -> [[location(0)]] vec4<f32> {
+	@fragment
+	fn main(@location(0) inColor : vec3<f32>) -> @location(0) vec4<f32> {
 		return vec4<f32>(inColor, 1.0);
 	}
 `;
 
 const VERTEX_SHADER = `
-	let positions : array<vec2<f32>, 3> = array<vec2<f32>, 3>(
+	const positions : array<vec2<f32>, 3> = array<vec2<f32>, 3>(
 		vec2<f32>(0.0, 0.5),
 		vec2<f32>(-0.5, -0.5),
 		vec2<f32>(0.5, -0.5)
 	);
 
-	let colors : array<vec3<f32>, 3> = array<vec3<f32>, 3>(
+	const colors : array<vec3<f32>, 3> = array<vec3<f32>, 3>(
 		vec3<f32>(1.0, 0.0, 0.0),
 		vec3<f32>(0.0, 1.0, 0.0),
 		vec3<f32>(0.0, 0.0, 1.0)
 	);
 
 	struct VertexOutput {
-		[[builtin(position)]] Position : vec4<f32>;
-		[[location(0)]] fragColor : vec3<f32>;
+		@builtin(position) Position : vec4<f32>,
+		@location(0) fragColor : vec3<f32>
 	};
   
-	[[stage(vertex)]]
-	fn main([[builtin(vertex_idx)]] VertexIndex : i32) -> VertexOutput {
+	@vertex
+	fn main(@builtin(vertex_index) VertexIndex : u32) -> VertexOutput {
 		var vertexOutput : VertexOutput;
 
 		vertexOutput.Position = vec4<f32>(positions[VertexIndex], 0.0, 1.0);
@@ -36,6 +36,12 @@ const VERTEX_SHADER = `
 	}
 `;
 
+const devicePixelRatio = window.devicePixelRatio || 1;
+const presentationSize = [
+	canvas.clientWidth * devicePixelRatio,
+	canvas.clientHeight * devicePixelRatio,
+];
+const sampleCount = 4;
 
 (async() => {
 	const adapter = await navigator.gpu.requestAdapter();
@@ -43,14 +49,12 @@ const VERTEX_SHADER = `
 	const device = await adapter.requestDevice();
 
 	const canvas = document.getElementById('canvas');
-	const context = canvas.getContext('gpupresent');
+	const context = canvas.getContext('webgpu');
 
-	/**
-	 * Swapchain
-	 */
-	const swapChain = context.configureSwapChain({
+	context.configure({
 		device,
-		format: SWAP_CHAIN_FORMAT
+		format: PRESENTATION_FORMAT,
+		alphaMode: 'opaque',
 	});
 
 	/**
@@ -67,6 +71,7 @@ const VERTEX_SHADER = `
 	console.log(await fragmentShaderModule.compilationInfo());
 
 	const pipeline = device.createRenderPipeline({
+		layout: 'auto',
 		vertex: {
 			module: vertexShaderModule,
 			entryPoint: 'main'
@@ -75,36 +80,48 @@ const VERTEX_SHADER = `
 			module: fragmentShaderModule,
 			entryPoint: 'main',
 			targets: [
-				{ format: SWAP_CHAIN_FORMAT }
+				{ format: PRESENTATION_FORMAT }
 			]
 		},
 		primitive: {
 			topology: 'triangle-list'
-		}
+		},
+		multisample: {
+			count: 4,
+		},
 	});
 
 	/**
 	 * Render
 	 */
 	const commandEncoder = device.createCommandEncoder();
-	const textureView = swapChain.getCurrentTexture().createView();
+	const texture = device.createTexture({
+		size: presentationSize,
+		sampleCount,
+		format: PRESENTATION_FORMAT,
+		usage: GPUTextureUsage.RENDER_ATTACHMENT,
+	});
+	const textureView = texture.createView();
 
 	const renderPassDescriptor = {
 		colorAttachments: [{
 			view: textureView,
-			loadValue: {
+			resolveTarget: context.getCurrentTexture().createView(),
+			clearValue: {
 				r: 0,
 				g: 0,
 				b: 0,
 				a: 1
 			},
+			loadOp: 'clear',
+			storeOp: 'discard',
 		}]
 	};
 
 	const pass = commandEncoder.beginRenderPass(renderPassDescriptor);
 	pass.setPipeline(pipeline);
 	pass.draw(3, 1, 0, 0);
-	pass.endPass();
+	pass.end();
 
 	device.queue.submit([commandEncoder.finish()]);
 })();
